2b9addc81f360f58ffab4dee6ff16f9826d1fff3
Terry Jung <51935603+tj9913@users.noreply.github.com>
2020-10-22 12:48:14 -0700

    Added code for output

--- demoCode/server.js
+++ demoCode/server.js
@@ -3,20 +3,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-//Testing for search. Necessary dependencies
-const fs = require('fs')
-const csv = require('csvtojson')//read file
-
-/*(async () => {
-    const recipeValues = csv().fromFile('interactions_test.csv')//load data
-    console.log(recipeValues)//outputs data
-
-})();
-//end testing*/
-
 app.use(express.json());
 app.use(express.static("express"));
 // default URL for website
+
+//dependencies for search
+const csvparser = require('csv-parser')
+const fs = require('fs')
 
 app.get('/response', (req, res) => {
     console.log("hello from server!")
@@ -32,10 +25,19 @@ app.use('/', function(req,res){
 
 app.get('/search', (req, res) => {
     console.log("hello from the search server!")
-    const recipeValues = csv().fromFile('interactions_test.csv')//load data
-    console.log(recipeValues)//outputs data
-    
-    return res.send(recipeValues);
+    //New code for search
+    const dataArray = [];//array storing data set
+    fs.createReadStream('interactions_test.csv')//read stream for file
+    .pipe(csvparser({}))
+    .on('data', (data) => dataArray.push(data))//store all the data in array
+    .on('end', () => {
+        console.log(dataArray[0]);//for now only output first five recipes
+        console.log(dataArray[1]);
+        console.log(dataArray[2]);
+        console.log(dataArray[3]);
+        console.log(dataArray[4]);
+    });
+    return res.send(dataArray);
 });
 
 
