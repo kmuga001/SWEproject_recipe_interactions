1cbdb491be0db6dd3a8e8479c9d3767fe992f4d9
Abel Theodros <atheo003@ucr.edu>
2020-10-24 14:45:58 -0600

    Sprint-2 final attempt, search bar fetch errors still remain

--- demoCode/server.js
+++ demoCode/server.js
@@ -7,6 +7,93 @@ app.use(express.json());
 app.use(express.static("express"));
 // default URL for website
 
+function csvToJSArray(data, delimeter) {
+
+  if (delimeter == undefined) 
+    delimeter = ',';
+  if (delimeter && delimeter.length > 1)
+    delimeter = ',';
+    console.log("data check:", data);
+    
+  var newline = '\n';             //newline
+  var eof = '';                   //end of file
+  var index = 0;                  //index will increment
+  var char = data.charAt(index);  //each individual character read
+  var row = 0;
+  var col = 0;
+  var array = new Array();        //return value
+
+  while (char != eof) {//make sure file did not end
+      while (char == '\t' || char == '\r' || char == ' ') {//check for tab and spaces
+          char = data.charAt(++index); // read next char
+      }
+    
+    var value = "";//value to be incremented
+    if (char == '\"') {// value enclosed by double-quotes
+      char = data.charAt(++index);
+        do {
+            if (char != '\"') {// no enclosed quotes and take in normal char
+                value += char;// increment value
+                char = data.charAt(++index);// move to next char
+        }
+            if (char == '\"') {// check for escaped double-quote
+          var cnext = data.charAt(i+1);
+          if (cnext == '\"') {// this is an escaped double-quote. 
+            // Add a double-quote to the value, and move two characters ahead.
+            value += '\"';
+              i += 2;
+              char = data.charAt(index);
+          }
+        }
+        }
+        while (char != eof && char != '\"');
+
+        if (char == eof) {
+        throw "data ended without quote";
+      }
+
+        char = data.charAt(++index);
+    }
+    else {
+      // value without quotes
+      while (char != eof && char != delimeter && char != newline) {//make sure char is a valid character and not end of file
+        value += char; //add char to value
+        char = data.charAt(++index); //increment to next char
+      }
+    }
+
+    // add the value to the array *******
+    if (array.length <= row) 
+      array.push(new Array());
+    array[row].push(value);
+    
+    // skip whitespaces
+    while (char == ' ' || char == '\t' || char == '\r') {
+      char = data.charAt(++i);
+    }
+   
+
+    // go to the next row or column *******
+    if (char == delimeter) {
+      // to the next column
+      col++;
+    }
+    else if (char == newline) { //******** */
+      // to the next row
+      col = 0;
+      row++;
+    }
+    else if (char != eof) {
+      // unexpected character
+      throw "Delimiter expected after character " + i;
+    }
+    
+    // go to the next character
+    char = data.charAt(++index);
+  } 
+  return array;
+}
+
 app.get('/response', (req, res) => {
     console.log("hello from server!")
     return res.send({
@@ -20,12 +107,12 @@ app.use('/', function(req,res){
   });
 
 app.get('/search', (req, res) => {
-    console.debug("hello from the search server!");
+    console.log("hello from the search server!");
 
     var filePath = './test.csv';
     var searchArray = new Array();
-    searchArray = csv2array(filePath, ',');
-    console.log(searchArray);
+    searchArray = csvToJSArray(filePath, ',');
+    console.log("array check:", searchArray);
 
     for(i = 0; i < searchArray.length(); i++){
         for(j = 0; j < searchArray[i].length(); j++){
@@ -38,103 +125,15 @@ app.get('/search', (req, res) => {
         }
 
     }
+    //temp output untill fetch issue is solved. - abel 
+    return res.send({
+      "array": "array"
+  })
+
+
 });
 
-function csv2array(data, delimeter) {
-    // Retrieve the delimeter
-    if (delimeter == undefined) 
-      delimeter = ',';
-    if (delimeter && delimeter.length > 1)
-      delimeter = ',';
-  
-    // initialize variables
-    var newline = '\n';             //newline
-    var eof = '';                   //end of file
-    var index = 0;                  //index will increment
-    var char = data.charAt(index);  //each individual character read
-    var row = 0;
-    var col = 0;
-    var array = new Array();        //return value
 
-    while (char != eof) {
-      // skip whitespaces
-        while (char == ' ' || char == '\t' || char == '\r') {
-            char = data.charAt(++index); // read next char
-        }
-      
-      // get value
-      var value = "";
-      if (char == '\"') {
-        // value enclosed by double-quotes
-        char = data.charAt(++index);
-        
-          do {
-              if (char != '\"') {
-                  // read a regular character and go to the next character
-                  value += char
-                  char = data.charAt(++index);
-          }
-
-              if (char == '\"') {
-            // check for escaped double-quote
-            var cnext = data.charAt(i+1);
-            if (cnext == '\"') {
-              // this is an escaped double-quote. 
-              // Add a double-quote to the value, and move two characters ahead.
-              value += '\"';
-                i += 2;
-                char = data.charAt(index);
-            }
-          }
-          }
-          while (char != eof && char != '\"');
-
-          if (char == eof) {
-          throw "data ended without quote";
-        }
-
-          char = data.charAt(++index);
-      }
-      else {
-        // value without quotes
-        while (char != eof && char != delimeter && char != newline) {
-          value += char;
-          char = data.charAt(++index);
-        }
-      }
-  
-      // add the value to the array *******
-      if (array.length <= row) 
-        array.push(new Array());
-      array[row].push(value);
-      
-      // skip whitespaces
-      while (char == ' ' || char == '\t' || char == '\r') {
-        char = data.charAt(++i);
-      }
-     
-  
-      // go to the next row or column *******
-      if (char == delimeter) {
-        // to the next column
-        col++;
-      }
-      else if (char == newline) { //******** */
-        // to the next row
-        col = 0;
-        row++;
-      }
-      else if (char != eof) {
-        // unexpected character
-        throw "Delimiter expected after character " + i;
-      }
-      
-      // go to the next character
-      char = data.charAt(++index);
-    }  
-    
-    return array;
-  }
 //
 
 const server = http.createServer(app);
